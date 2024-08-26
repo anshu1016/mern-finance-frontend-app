d7d66bd2a686e57fe545bfb5d6e4687ed681c4bf
anshu1016 <arunshukla98710@gmail.com>
2024-08-26 19:05:04 +0530

    PWA DEPLOYED SUCCESSFULLY!

--- vite.config.ts
+++ vite.config.ts
@@ -15,12 +15,12 @@ export default defineConfig({
         background_color: '#ffffff',
         icons: [
           {
-            src: 'images/screenshot.png',
+            src: 'images/image (1).png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: 'images/screenshot2.png',
+            src: 'images/image (2).png',
             sizes: '512x512',
             type: 'image/png',
           },
